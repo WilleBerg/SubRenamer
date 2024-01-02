74a54f6d7e3fa626bc90712ef1516b932a92876e
jammy <william.berg2@gmail.com>
2024-01-02 13:54:43 +0100

    minor changes

--- make_test_files.sh
+++ make_test_files.sh
@@ -6,6 +6,11 @@ touch ./test/mov_testS01E02.mov
 touch ./test/mov_testS01E03.mov
 touch ./test/mov_testS01E04.mov
 touch ./test/mov_testS01E05.mov
+touch ./test/mov_testS02E01.mov
+touch ./test/mov_testS02E02.mov
+touch ./test/mov_testS02E03.mov
+touch ./test/mov_testS02E04.mov
+touch ./test/mov_testS02E05.mov
 
 
 touch ./test/sub_testS01E01.srt
@@ -13,4 +18,12 @@ touch ./test/sub_testS01E02.srt
 touch ./test/sub_testS01E03.srt
 touch ./test/sub_testS01E04.srt
 touch ./test/sub_testS01E05.srt
+touch ./test/sub_testS02E01.srt
+touch ./test/sub_testS02E02.srt
+touch ./test/sub_testS02E03.srt
+touch ./test/sub_testS02E04.srt
+touch ./test/sub_testS02E05.srt
+
+touch ./test/nothing1
+touch ./test/nothing2
 echo "Test files created."
